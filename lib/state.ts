d16fa18e29c9271af5a4408a49e13c2c7792600c
no <foo@example.com>
2015-06-04 15:00:16 +0900

    use array for status table

--- lib/state.ts
+++ lib/state.ts
@@ -1,32 +1,43 @@
 import * as pathutil from './pathutil';
+import PathSeg = pathutil.PathSeg;
+
+interface PathEntry<T>{
+    seg:PathSeg;
+    next:State<T>;
+}
 export default class State<T>{
-    private table:{
-        [seg:string]:State<T>
-    };
+    private table:Array<PathEntry<T>>;
     private value:T;
     constructor(){
-        this.table={};
+        this.table=[];
         this.value=void 0;
     }
 
     //find
-    find(segs:Array<pathutil.PathSeg>,make?:boolean):State<T>{
+    find(segs:Array<PathSeg>,make?:boolean):State<T>{
         if(segs.length===0){
             return this;
         }
         var seg=segs[0];
-        var res=this.table[seg];
-        if(res==null){
-            if(make===true){
-                //init new state
-                res=new State<T>();
-                this.table[seg]=res;
-                return res.find(segs.slice(1),make);
-            }else{
-                return null;
+        for(let i=0,table=this.table,l=table.length;i<l;i++){
+            let e=table[i];
+            if(e.seg===seg){
+                //match
+                return e.next.find(segs.slice(1),make);
             }
         }
-        return res.find(segs.slice(1),make);
+        //no match
+        if(make===true){
+            //init new state
+            let res=new State<T>();
+            this.table.push({
+                seg:seg,
+                next:res
+            });
+            return res.find(segs.slice(1),make);
+        }else{
+            return null;
+        }
     }
     //get value
     get():T{
